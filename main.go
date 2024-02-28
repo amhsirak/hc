cec5ad827b20c2695794c441bea0d130452f6785
karishmas6 <carishmashukla28@gmail.com>
2024-02-29 05:03:03 +0530

    toyinggg

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	url := ""
+
+	client := http.Client{}
+	response, err := client.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(string(bytes))
+}
