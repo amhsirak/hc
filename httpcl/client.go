ef51d21e141c8cc9f3b036dfcaed048464b60f1b
karishmas6 <carishmashukla28@gmail.com>
2024-03-04 23:20:40 +0530

    feat: call methods in HttpClient interface

--- httpcl/client.go
+++ httpcl/client.go
@@ -9,7 +9,13 @@ func New() HttpClient {
 	return client
 }
 
-type HttpClient interface{}
+type HttpClient interface{
+	Get(url string, headers http.Header) (*http.Response, error)
+	Post(url string, headers http.Header, body interface{}) (*http.Response, error)
+	Put(url string, headers http.Header, body interface{}) (*http.Response, error)
+	Patch(url string, headers http.Header, body interface{}) (*http.Response, error)
+	Delete(url string, headers http.Header) (*http.Response, error)
+}
 
 func (c *httpClient) Get(url string, headers http.Header) (*http.Response, error) {
 	return c.do(http.MethodGet, url, headers, nil)
