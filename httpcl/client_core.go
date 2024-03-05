37776015858d92654bd6522849e4281b115a6a16
karishmas6 <carishmashukla28@gmail.com>
2024-03-05 21:30:27 +0530

    fix: remove Header

--- httpcl/client_core.go
+++ httpcl/client_core.go
@@ -19,18 +19,18 @@ func (c *httpClient) do(method string, url string, headers http.Header, body int
 func (c *httpClient) getRequestHeaders(requestHeaders http.Header) http.Header {
 	result := make(http.Header)
 
-	// Default | Common Headers For Request 
+	// default and common headers for request 
 	for header, value := range c.Headers {
 		if len(value) > 0 {
-			result.Header.Set(header, value[0])
+			result.Set(header, value[0])
 		}
 	}
 
-	// Custom Headers For Request
-	// headers is of type Header map[string][]string
+	// custom headers for request
+	// requestHeaders is of type Header map[string][]string
 	for header, value := range requestHeaders {
 		if len(value) > 0 {
-			result.Header.Set(header, value[0])
+			result.Set(header, value[0])
 		}
 	}
 	return result
