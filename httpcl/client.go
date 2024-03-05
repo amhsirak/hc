cd5d732c1755aab50cd7400b78e0efc1ce36302a
karishmas6 <carishmashukla28@gmail.com>
2024-03-05 21:03:51 +0530

    fix: send body instead of nil

--- httpcl/client.go
+++ httpcl/client.go
@@ -22,15 +22,15 @@ func (c *httpClient) Get(url string, headers http.Header) (*http.Response, error
 }
 
 func (c *httpClient) Post (url string, headers http.Header, body interface{}) (*http.Response, error) {
-	return c.do(http.MethodPost, url, headers, nil)
+	return c.do(http.MethodPost, url, headers, body)
 }
 
 func (c *httpClient) Put (url string, headers http.Header, body interface{}) (*http.Response, error) {
-	return c.do(http.MethodPut, url, headers, nil)
+	return c.do(http.MethodPut, url, headers, body)
 }
 
 func (c *httpClient) Patch (url string, headers http.Header, body interface{}) (*http.Response, error) {
-	return c.do(http.MethodPatch, url, headers, nil)
+	return c.do(http.MethodPatch, url, headers, body)
 } 
 
 func (c *httpClient) Delete(url string, headers http.Header) (*http.Response, error) {
