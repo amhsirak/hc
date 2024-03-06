4bcda4c4fb42c06bef83618bed9dfb4056225e48
karishmas6 <carishmashukla28@gmail.com>
2024-03-06 20:20:55 +0530

    test: common header

--- example.go
+++ example.go
@@ -4,23 +4,36 @@ import (
 	"fmt"
 	"github.com/karishmashuklaa/hc/httpcl"
 	"net/http"
+	"io/ioutil"
 )
+
+var (
+	githubHttpClient = getGithubClient()
+)
+
+func getGithubClient() httpcl.HttpClient {
+	client := httpcl.New()
+
+	commonHeaders := make(http.Header)
+	commonHeaders.Set("Authorization", "Bearer ABC-123")
+	client.SetHeaders(commonHeaders)
+
+	return client
+}
 
 func main() {
 	getUrls()
 }
 
 func getUrls() {
-	client := httpcl.New()
-
-	headers := make(http.Header)
-	headers.Set("Authorization", "Bearer ABC-123")
-
-	response, err := client.Get("https://api.github.com", headers)
+	response, err := githubHttpClient.Get("https://api.github.com", nil)
 
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println(response.StatusCode)
+
+	bytes, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(bytes))
 }
