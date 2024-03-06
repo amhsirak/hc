5e39bb47e520ba63073de5cafc64267d5dac93a3
karishmas6 <carishmashukla28@gmail.com>
2024-03-06 23:07:32 +0530

    test: validate TestGetRequestHeaders

--- httpcl/client_core_test.go
+++ httpcl/client_core_test.go
@@ -5,10 +5,20 @@ import (
 	"testing"
 )
 
+// flow of tests: initalization, execution, validation
+
 func TestGetRequestHeaders(t *testing.T) {
 	client := httpClient{}
 	commonHeaders := make(http.Header)
 	commonHeaders.Set("Content-Type", "application/json")
 	commonHeaders.Set("User-Agent", "test-httpcl")
 	client.Headers = commonHeaders
+
+	requestHeaders := make(http.Header)
+	requestHeaders.Set("X-Request-Id", "ABC-999")
+	finalHeaders := client.getRequestHeaders(requestHeaders)
+
+	if len (finalHeaders) != 3 {
+		t.Error("Expected 3 headers, got ", len(finalHeaders))
+	}
 }
