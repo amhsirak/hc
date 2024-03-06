6877aa0ffbf7e0bb182a54aee389f1deeee18229
karishmas6 <carishmashukla28@gmail.com>
2024-03-07 01:10:15 +0530

    test: req body json

--- httpcl/client_core_test.go
+++ httpcl/client_core_test.go
@@ -51,4 +51,18 @@ func TestGetRequestBody(t *testing.T) {
 		}
 	})
 
+	t.Run("BodyWithJson", func(t *testing.T) {
+		requestBody := []string{"one", "two"}
+
+		body, err := client.getRequestBody("application/json", requestBody)
+
+		if err != nil {
+			t.Error("no error expected when marshaling slice as json")
+		}
+
+		if string(body) != `["one","two"]` {
+			t.Error("invalid json body obtained")
+		}
+	})
+
 }
